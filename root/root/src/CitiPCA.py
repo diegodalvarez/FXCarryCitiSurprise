3651c43092b22846652647a62c30df0246d3e14d
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2025-01-19 15:04:08 -0500

    median_mean_conditioned_raw

---
+++ root/root/src/CitiPCA.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 21:39:43 2024
+
+@author: diegoalvarez
+
+"""
+import os
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+from   sklearn.decomposition import PCA
+from   FXCarryDataCollect import FXData
+
+class PCAModel(FXData): 
+    
+    def __init__(self) -> None: 
+        
+        super().__init__()
+        self.pca_data  = os.path.join(self.data_path, "PCAData")
+        
+        if os.path.exists(self.pca_data) == False: os.makedirs(self.pca_data)
+        
+    def _prep_data(self) -> pd.DataFrame: 
+        
+        df_surprise  = self.get_citi_suprise()
+        good_tickers = (df_surprise.dropna().drop(
+            columns = ["value"]).
+            groupby("security").
+            agg("count").
+            assign(compare_value = lambda x: x.date.median() * 0.8).
+            query("date >= compare_value").
+            index.
+            to_list())
+        
+        df_wider = (df_surprise.query(
+            "security == @good_tickers").
+            pivot(index = "date", columns = "security", values = "value").
+            dropna())
+        
+        return df_wider
+    
+    def pca(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.pca_data, "CitiSurprisePCA.parquet")
+
+        try:
+            
+            if verbose == True: print("Trying to find Citi Surprise PCA Return Data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data now collecting it")
+        
+            df_wider  = self._prep_data()
+            pca_model = PCA(n_components = len(df_wider.columns))
+            
+            df_fitted_values = (pd.DataFrame(
+                data    = pca_model.fit_transform(df_wider),
+                index   = df_wider.index,
+                columns = ["PC{}".format(i + 1) for i in range(len(df_wider.columns))]).
+                reset_index().
+                melt(id_vars = "date").
+                rename(columns = {"variable": "pc"}))
+            
+            df_var = (pd.DataFrame(
+                data    = pca_model.fit(df_wider).explained_variance_ratio_,
+                columns = ["explained_variance_ratio"],
+                index   = ["PC{}".format(i + 1) for i in range(len(df_wider.columns))]).
+                reset_index().
+                rename(columns = {"index": "pc"}))
+            
+            df_out = (df_fitted_values.merge(
+                right = df_var, how = "inner", on = ["pc"]))
+            
+            if verbose == True: print("Saving PCA data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+    
+    def full_sample_ols_index_backtest(self, verbose: bool = False) -> tuple: 
+        
+        signal_path = os.path.join(self.pca_data, "PCAIndexFullSampleOLSBacktestRtn.parquet")
+        param_path  = os.path.join(self.pca_data, "PCAIndexFullSampleOLSBacktestParams.parquet")
+
+        try:
+            
+            if verbose == True: print("Trying to find PCA OLS Model Data")
+            df_signal = pd.read_parquet(path = signal_path, engine = "pyarrow")
+            df_params = pd.read_parquet(path = param_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data now collecting it")
+        
+            df_pca = self.pca()
+            df_rtn = (self.get_misc_indices().drop(
+                columns = ["security", "clean_val", "raw_val"]).
+                dropna())
+            
+            pcs = df_pca.pc.drop_duplicates().to_list()
+            
+            df_signal, df_params = pd.DataFrame(), pd.DataFrame()
+            
+            for i in range(1, len(pcs) + 1):
+                
+                pcs      = ["PC{}".format(j + 1) for j in range(i)]
+                df_wider = (df_pca.query(
+                    "pc == @pcs").
+                    drop(columns = ["explained_variance_ratio"]).
+                    pivot(index = "date", columns = "pc", values = "value").
+                    merge(right = df_rtn, how = "inner", on = ["date"]).
+                    set_index("date"))
+                
+                model = (sm.OLS(
+                    endog = df_wider.rtn,
+                    exog  = sm.add_constant(df_wider.drop(columns = ["rtn"]))).
+                    fit())
+                
+                df_signal_tmp = (model.resid.to_frame(
+                    name = "lag_resid").
+                    shift().
+                    dropna().
+                    merge(right = df_rtn, how = "inner", on = ["date"]).
+                    assign(pcs = i))
+                
+                df_param_tmp = (model.params.to_frame(
+                    name = "param").
+                    assign(
+                        pvalue = model.pvalues,
+                        pcs    = i))
+                
+                df_signal = pd.concat([df_signal, df_signal_tmp])
+                df_params = pd.concat([df_params, df_param_tmp]) 
+            
+            if verbose == True: print("Saving data")
+            df_signal.to_parquet(path = signal_path, engine = "pyarrow")
+            df_params.to_parquet(path = param_path, engine = "pyarrow")
+            
+        return df_signal, df_params
+        
+def main() -> None: 
+        
+    PCAModel().pca(verbose = True)
+    PCAModel().full_sample_ols_index_backtest(verbose = True)
+    
+if __name__ == "__main__": main()
---
+++ root/root/src/FXCarryDataCollect.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 15:04:06 2024
+
+@author: diegoalvarez
+"""
+import os
+import numpy as np
+import pandas as pd
+
+class FXData:
+
+    def __init__(self) -> None: 
+        
+        self.root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        self.repo_path = os.path.abspath(os.path.join(self.root_path, os.pardir))
+        self.data_path = os.path.join(self.repo_path, "data")
+        self.raw_path  = os.path.join(self.data_path, "RawData")
+        
+        if os.path.exists(self.data_path) == False: os.makedirs(self.data_path)
+        if os.path.exists(self.raw_path)  == False: os.makedirs(self.raw_path)
+        
+        self.bbg_raw_path = r"/Users/diegoalvarez/Desktop/BBGData"
+        if os.path.exists(self.bbg_raw_path) == False: 
+            self.bbg_raw_path = r"C:\Users\Diego\Desktop\app_prod\BBGData"
+        
+        self.bbg_ticker_path = os.path.join(self.bbg_raw_path, "root", "BBGTickers.xlsx")
+        self.df_tickers      = (pd.read_excel(io = self.bbg_ticker_path))
+        
+    def get_carry_return(self, verbose: bool = False) -> pd.DataFrame:
+        
+        file_path = os.path.join(self.raw_path, "FXCarryReturn.parquet")
+
+        try:
+            
+            if verbose == True: print("Trying to find FX Carry Return Data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data now collecting it")
+        
+            tickers = (self.df_tickers.query(
+                "Category == 'Currencies' & Subcategory == 'Return Indices'").
+                assign(Security = lambda x: x.Security.str.split(" ").str[0]).
+                Security.
+                to_list())
+            
+            paths = [
+                os.path.join(self.bbg_raw_path, "data", ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = paths, engine = "pyarrow").
+                assign(
+                    date     = lambda x: pd.to_datetime(x.date).dt.date,
+                    security = lambda x: x.security.str.split(" ").str[0]).
+                drop(columns = ["variable"]))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+    
+    def get_citi_suprise(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.raw_path, "CitiSurprise.parquet")
+
+        try:
+            
+            if verbose == True: print("Trying to find Citi Surprise Data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data now collecting it")
+    
+            tickers = (self.df_tickers.query(
+                "Category == 'Index/Stats' & Subcategory == 'Equity Index'").
+                assign(
+                    Security = lambda x: x.Security.str.split(" ").str[0],
+                    ending   = lambda x: x.Description.str.split(" ").str[0]).
+                query("ending == 'Citi'").
+                Security.
+                drop_duplicates().
+                to_list())
+            
+            paths = [
+                os.path.join(self.bbg_raw_path, "data", ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = paths, engine = "pyarrow").
+                assign(
+                    security = lambda x: x.security.str.split(" ").str[0],
+                    date     = lambda x: pd.to_datetime(x.date).dt.date).
+                drop(columns = ["variable"]))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+    
+    def get_misc_indices(self, verbose: bool = False) -> pd.DataFrame:
+        
+        
+        file_path = os.path.join(self.raw_path, "MiscIndices.parquet")
+
+        try:
+            
+            if verbose == True: print("Trying to find Citi Surprise Data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data now collecting it")
+        
+            tickers = ["FXCARRSP"]
+            paths   = [
+                os.path.join(self.bbg_raw_path, "data", ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = paths, engine = "pyarrow").
+                assign(
+                    security = lambda x: x.security.str.split(" ").str[0],
+                    date     = lambda x: pd.to_datetime(x.date).dt.date).
+                groupby("security").
+                apply(self._double_clean).
+                drop(columns = ["security"]).
+                reset_index().
+                drop(columns = ["first_clean"]).
+                rename(columns = {
+                    "second_clean": "clean_val",
+                    "raw_value"   : "raw_val"}))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+    
+    def _double_clean(
+            self,
+            df           : pd.DataFrame,
+            long_replace : int = 20, 
+            long_window  : int = 200, 
+            short_replace: int = 5, 
+            short_window : int = 30) -> pd.DataFrame:
+    
+        df_out = (df.
+            assign(security = lambda x: x.security.str.split(" ").str[0]).
+            assign(
+                short_mean  = lambda x: x.value.rolling(window = 20).mean(),
+                roll_mean   = lambda x: x.value.rolling(window = 200).mean(),
+                roll_std    = lambda x: x.value.rolling(window = 200).std(),
+                z_score     = lambda x: np.abs((x.value - x.roll_mean) / x.roll_std),
+                first_clean = lambda x: np.where(x.z_score > 4, x.short_mean, x.value)).
+            set_index("date")
+            [["value", "first_clean", "security"]].
+            assign(
+                short_mean  = lambda x: x.first_clean.rolling(window = 5).mean(),
+                roll_mean   = lambda x: x.first_clean.rolling(window = 30).mean(),
+                roll_std    = lambda x: x.first_clean.rolling(window = 30).std(),
+                z_score     = lambda x: np.abs((x.value - x.roll_mean) / x.roll_std),
+                second_clean = lambda x: np.where(x.z_score > 2.5, x.short_mean, x.first_clean))
+            [["value", "security", "first_clean", "second_clean"]].
+            rename(columns = {"value": "raw_value"}).
+            sort_index().
+            assign(rtn = lambda x: x.second_clean.pct_change()))
+        
+        return df_out
+  
+def main() -> None:       
+        
+    df = FXData().get_carry_return(verbose = True)
+    df = FXData().get_citi_suprise(verbose = True)
+    df = FXData().get_misc_indices(verbose = True)
+    
+if __name__ == "__main__": main()
